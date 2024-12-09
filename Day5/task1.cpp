84fc001e695a89c54c81d86cc96767218e30f63b
Michael JS <michaelsand155@gmail.com>
2024-12-09 20:43:34 +1000

    day5 v1

--- Day5/task1.cpp
+++ Day5/task1.cpp
@@ -8,11 +8,14 @@ struct Key{
     int after;
 };
 
+
+
 int main(){
     std::vector<Key> keyData;
+    std::vector<int> sortedKeyData;
     std::vector<std::vector<int>> pageData;
     std::string line;
-    std::ifstream file("input1small.txt");
+    std::ifstream file("input.txt");
 
     while(std::getline(file, line)){
         if(line == "") break;
@@ -31,9 +34,87 @@ int main(){
             }
             pageData[i].push_back(std::stoi(line.substr(0, commaPos)));
             line.erase(0, commaPos+1);
-
         }
     }
+
+    sortedKeyData.push_back(keyData[0].before);
+    sortedKeyData.push_back(keyData[0].after);
+
+    for(int i = 1; i < keyData.size(); i++){
+        int beforePos = -1;
+        int afterPos = -1;
+        for(int j = 0; j < sortedKeyData.size(); j++){
+            if(sortedKeyData[j] == keyData[i].before) beforePos = j;
+            if(sortedKeyData[j] == keyData[i].after) afterPos = j;
+        }
+
+        if(beforePos == -1 && afterPos == -1){ // if neither are in sorted list
+            sortedKeyData.push_back(keyData[i].before);
+            sortedKeyData.push_back(keyData[i].after);
+            continue;
+        }
+
+        if(beforePos != -1 && afterPos == -1){ // if before is in sorted list
+            sortedKeyData.push_back(keyData[i].after);
+            continue;
+        }
+
+        if(beforePos == -1 && afterPos != -1){ // if after is in sorted list
+            std::vector<int>::iterator it = sortedKeyData.begin();
+            sortedKeyData.insert(it+afterPos, keyData[i].before);
+            continue;
+        }
+
+        if(beforePos != -1 && afterPos != -1){ // if both are in sorted list
+
+            if(beforePos < afterPos) continue;
+
+            std::vector<int>::iterator it = sortedKeyData.begin();
+            sortedKeyData.erase(it+beforePos);
+            it = sortedKeyData.begin();
+            sortedKeyData.insert(it+afterPos, keyData[i].before);
+            continue;
+        }
+    }
+    //97 75 47 61 53 29 13
+    //75, 47, 61, 53, 29
+    //97, 61, 53, 29, 13
+
+    // page data, grab item and check left, find key value and follow right. if data from right of key data is present left of page data, it's bad
+
+    int total = 0;
+
+    for(int i = 0; i < pageData.size(); i++){
+        bool isSorted = true;
+        for(int j = pageData[i].size()-1; j > 0; j--){
+            if(isSorted == false) break;
+            int keyPos = -1;
+            for(int k = 0; k < sortedKeyData.size(); k++){
+                if(pageData[i][j] == sortedKeyData[k]){
+                    keyPos = k;
+                    break;
+                }
+            }
+            if(keyPos == -1) continue;
+
+            for(int k = keyPos; k < sortedKeyData.size(); k++){
+                for(int l = j-1; l >= 0; l--){
+                    if(sortedKeyData[k] == pageData[i][l]){
+                        isSorted = false;
+                        break;
+                    }
+                }
+                if(isSorted == false) break;
+            }
+        }
+
+        if(isSorted){
+            std::cout << pageData[i][pageData[i].size()/ 2] << std::endl;
+            total += pageData[i][pageData[i].size()/ 2];
+        }
+    }
+
+    std::cout << total << std::endl;
 
     return(0);
 }
